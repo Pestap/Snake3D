9bebdb705ea2f56e5f1bb459aae27f96a233109f
Pestap <90579181+Pestap@users.noreply.github.com>
2022-12-22 00:48:26 +0100

    Turning based on direction of movement

--- snake_js/snakePart.js
+++ snake_js/snakePart.js
@@ -12,24 +12,21 @@ export class SnakePart{
     }
 
     move(direction){
-        console.log(direction)
         if(direction === 'w'){
-            this.cube.position.add(new THREE.Vector3( 0, 0, -1 ));
-            this.direction = new THREE.Vector3( 0, 0, -1 );
+            this.cube.position.add(this.direction);
         }else if(direction === 's'){
             this.cube.position.add(new THREE.Vector3( 0, 0, 1 ));
             this.direction = new THREE.Vector3( 0, 0, 1 );
         }else if(direction === 'a'){
-            this.cube.position.add(new THREE.Vector3( -1, 0,0 ));
-            this.direction = new THREE.Vector3( -1, 0, 0 );
+            this.cube.position.add(this.direction);
+            this.direction = new THREE.Vector3(this.direction.z, 0,-this.direction.x);
         }else if(direction === 'd'){
-            this.cube.position.add(new THREE.Vector3( 1, 0, 0 ));
-            this.direction = new THREE.Vector3( 1, 0, 0 )
+            this.cube.position.add(this.direction);
+            this.direction = new THREE.Vector3(-this.direction.z, 0,this.direction.x);
         }
     }
 
     setPosition(vector){
-        
         this.cube.position.set(vector.x, vector.y, vector.z);   
     }
 
