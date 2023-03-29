68d9bc778f11fee9a1b663b21325e46ef83497ad
Pestap <90579181+Pestap@users.noreply.github.com>
2023-03-29 12:19:33 +0200

    Created backend project

---
+++ snake-backend/src/test/java/pestap/snakebackend/SnakeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pestap.snakebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
