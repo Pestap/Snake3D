987c5786503aebe83380181accc10c0a151b8b2a
Pestap <90579181+Pestap@users.noreply.github.com>
2023-03-31 23:57:39 +0200

    Implemented REST controller for POST request to add Player object

--- snake-backend/src/main/java/pestap/snakebackend/player/controller/PlayerController.java
+++ snake-backend/src/main/java/pestap/snakebackend/player/controller/PlayerController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
+import pestap.snakebackend.player.dto.CreatePlayerRequest;
 import pestap.snakebackend.player.dto.GetPlayerResponse;
+import pestap.snakebackend.player.entity.Player;
 import pestap.snakebackend.player.service.PlayerService;
 
 @RestController
@@ -28,8 +30,13 @@ public class PlayerController {
 
     @PostMapping
     public ResponseEntity<Void> createPlayer(@RequestBody CreatePlayerRequest request, UriComponentsBuilder builder){
+        Player playerToAdd = CreatePlayerRequest.dtoToEntityMapper().apply(request);
+        playerToAdd = playerService.createPlayer(playerToAdd);
 
-
+        return ResponseEntity.created(builder
+                .pathSegment("api", "players", "{username}")
+                .buildAndExpand(playerToAdd.getUsername()).toUri())
+                .build();
 
     }
 
