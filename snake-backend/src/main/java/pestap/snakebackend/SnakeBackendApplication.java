cd84cc93e2b7b02bab729e17464ca01b5b2d5abc
Pestap <90579181+Pestap@users.noreply.github.com>
2023-04-26 18:03:19 +0200

    Cleanup

--- snake-backend/src/main/java/pestap/snakebackend/SnakeBackendApplication.java
+++ snake-backend/src/main/java/pestap/snakebackend/SnakeBackendApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SnakeBackendApplication {
 
     public static void main(String[] args) {
-        System.out.println("SIEMA");
         SpringApplication.run(SnakeBackendApplication.class, args);
     }
 
